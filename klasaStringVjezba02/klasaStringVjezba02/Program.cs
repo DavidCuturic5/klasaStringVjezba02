5eace454fc539c428092f21cbc77d494323ce710
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-21 13:39:51 +0200

    Add files via upload

---
+++ klasaStringVjezba02/klasaStringVjezba02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVjezba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova");
+            string unos = Console.ReadLine();
+            string ispis = unos.Replace(" ", "_");
+            Console.WriteLine(ispis);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
